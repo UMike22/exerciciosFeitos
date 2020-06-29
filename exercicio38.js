bf239f7828bb0b3b494c0dc20223ffc62c842872
UMike22 <kaelvieira@gmail.com>
2020-06-29 18:18:16 -0300

    exercicioResolvido

--- exercicio38.js
+++ exercicio38.js
@@ -1,6 +1,11 @@
-function Intervalo(i,fim){
+function ImprimirPares(inicio = 0,fim = 100){
     let Impares = []
     let Pares = []
+    if(inicio>fim){
+        inicio = inicio+fim
+        fim = inicio-fim
+        inicio = inicio-fim
+    }
     for(i=0;i<fim;i++){
         if(i%2==0){
         Pares.push(i)
@@ -12,4 +17,4 @@ function Intervalo(i,fim){
     }
 
 
-console.log(Intervalo(0,100))
+console.log(ImprimirPares(10,50))
